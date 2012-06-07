696b179957eabc0976709612015205aff7018aae
Mike Deats <mikedeats@gmail.com>
2012-06-06 22:20:42 -0600

    added 3rd NTP server to prevent server instability

--- cookbooks/ntp/attributes/default.rb
+++ cookbooks/ntp/attributes/default.rb
@@ -21,7 +21,7 @@
 
 # default attributes for all platforms
 default[:ntp][:is_server] = false
-default[:ntp][:servers]   = ["0.pool.ntp.org", "1.pool.ntp.org"]
+default[:ntp][:servers]   = ["0.pool.ntp.org", "1.pool.ntp.org", "2.pool.ntp.org"]
 default[:ntp][:driftfile] = "/var/lib/ntp/ntp.drift"
 default[:ntp][:statsdir] = "/var/log/ntpstats/"
 
